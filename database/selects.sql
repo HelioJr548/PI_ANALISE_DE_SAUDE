edec3d9ba2fab4565178a02eaffba18aa2a63927
Helio de Jesus dos Santos Junior <84392613+HelioJr548@users.noreply.github.com>
2023-12-05 19:51:41 -0300

    Update selects.sql

--- database/selects.sql
+++ database/selects.sql
@@ -1,5 +1,5 @@
 -- Busca Simples
-SELECT * FROM doença;
+SELECT * FROM doenca;
 SELECT * FROM equipe_saude;
 SELECT * FROM paciente;
 SELECT * FROM tratamento;
@@ -36,7 +36,7 @@ WHERE DATE(data_registro) = '2023-12-01';
 SELECT p.nome AS paciente, d.nome AS doenca, t.nome AS tratamento, rm.data_registro
 FROM registro_medico rm
 JOIN paciente p ON rm.paciente_id = p.id
-JOIN doença d ON rm.doença_id = d.id
+JOIN doenca d ON rm.doenca_id = d.id
 JOIN tratamento t ON rm.tratamento_id = t.id
 WHERE p.id = 1; 
 
@@ -44,6 +44,6 @@ WHERE p.id = 1;
 SELECT p.nome AS nome_paciente, d.nome AS doenca, r.data_registro
 FROM registro_medico r
 JOIN paciente p ON r.paciente_id = p.id
-JOIN doença d ON r.doença_id = d.id
+JOIN doenca d ON r.doenca_id = d.id
 WHERE d.nome = 'Gripe'; 
 
